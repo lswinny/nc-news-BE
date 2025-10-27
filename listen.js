a4132c50bac44e5fb6d7711702b4019282ff32be
Lydia <lswinny@gmail.com>
2025-10-27 17:39:24 +0000

    change to listen.js file

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require("./middlewear/app")
+const app = require("./app")
 const { PORT = 9090 } = process.env.PORT || 9090
 
 app.listen(PORT, () => console.log("Server is listening on port ${PORT}..."));
