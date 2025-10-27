2d21b49ecf1b9e3913a56a911c01c377c6112e65
Lydia <lswinny@gmail.com>
2025-10-27 15:24:48 +0000

    Fixed small error in topics and users

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,9 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
+if(ENV !== "production") {
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("No PGDATABASE or DATABASE_URL configured");
@@ -13,9 +15,6 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-console.log(config);
 const db = new Pool(config);
-
-
 
 module.exports = db;
